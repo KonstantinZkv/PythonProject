690fda6db1ae762027a4ad8ff354c62e6fda575e
Konstantin Zakovryazhin <zkostyan94@gmail.com>
2025-04-03 23:27:28 +0700

    fix tests

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,13 +3,16 @@ import pytest
 from src.widget import get_date, mask_account_card
 
 
-@pytest.mark.parametrize("input_string, expected", [
-    ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
-    ("Maestro 7000792289606361", "Maestro 7000 79** **** 6361"),
-    ("Счет 35383033474447895560", "Счет **5560"),
-    ("Счет 12345678901234567890", "Счет **7890"),
-    ("Счет 98765432109876543210", "Счет **3210"),
-])
+@pytest.mark.parametrize(
+    "input_string, expected",
+    [
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+        ("Maestro 7000792289606361", "Maestro 7000 79** **** 6361"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Счет 12345678901234567890", "Счет **7890"),
+        ("Счет 98765432109876543210", "Счет **3210"),
+    ],
+)
 def test_mask_account_card(input_string, expected):
     """Тестирование функции обработки информации о картах и счетах."""
     assert mask_account_card(input_string) == expected
